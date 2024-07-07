1e748c14cf190b94d0a720a31621d8d7d9516b6e
KalminX <noriabure@gmail.com>
2024-07-07 14:19:54 +0100

    updated secret key

--- user_auth_and_org/user_auth_and_org/deployment.py
+++ user_auth_and_org/user_auth_and_org/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 CSRF_trusted_origins = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
