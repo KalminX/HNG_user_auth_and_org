a69b648b9fbba7bd920c79df5b4dd50de7692d4a
Numerous Oriabure <118700541+KalminX@users.noreply.github.com>
2024-07-07 16:24:32 +0100

    Update deployment.py

--- user_auth_and_org/user_auth_and_org/deployment.py
+++ user_auth_and_org/user_auth_and_org/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 CSRF_trusted_origins = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
+DEBUG = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
